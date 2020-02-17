69eaf03c27e8dd6010eed811df8d9cc9976223d4
TUREVICH <z.turevich@mail.ru>
2020-02-17 18:45:22 +0300

    change Task: 6

--- src/com/company/Task6.java
+++ src/com/company/Task6.java
@@ -2,14 +2,10 @@ package com.company;
 
 public class Task6 {
 
-    int day;
-    int month;
-    int year;
-
     public void task6() {
-         day = 31;
-         month = 12;
-         year = 2020;
+        int day = 28;
+        int month = 2;
+        int year = 2020;
         int dayMax;
 
 
@@ -31,19 +27,15 @@ public class Task6 {
             day = 1;
             month = 1;
             year++;
-            sout();
         } else if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10) && (day == 31)) {
             day = 1;
             month++;
-            sout();
         } else if ((month == 4 || month == 6 || month == 9 || month == 11) && (day == 30)) {
             day = 1;
             month++;
-            sout();
         } else if ( month == 2 && day == dayMax) {
             day = 1;
             month++;
-            sout();
         } else {
 
             if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && (day > 31)) {
@@ -56,9 +48,6 @@ public class Task6 {
                 day++;
             }
         }
-    }
-
-    private void sout(){
         System.out.println(day + "." + month + "." + year);
     }
 }
--- src/com/company/Task7.java
+++ src/com/company/Task7.java
@@ -3,7 +3,7 @@ package com.company;
 public class Task7 {
     int a = 3;
     int b = 11;
-    int c = 5;
+    int c = 6;
     int d = 3;
     int e = 5;
     int f = 15;
@@ -11,18 +11,18 @@ public class Task7 {
 
     public void task7() {
 
-        if ((((a + c) <= e) && (b <= f && d <= f)) || (((b + d) <= f) && (a <= e && c <= e)) || (((b + c) <= e) && (a <= f && d <= f)) || (((a + d) <= e) && (b <= f && c <= f)) || (((a + d) <= f) && (b <= e && c <= e)) || (((b + c) <= f) && (a <= e && d <= e)) || (((a + c) <= f) && (b <= e && d <= e)) || (((b + d) <= e) && (a <= f && c <= f))){
+        if ((((a + c) <= e) && (b <= f && d <= f)) || (((b + d) <= f) && (a <= e && c <= e)) || (((b + c) <= e) && (a <= f && d <= f)) || (((a + d) <= e) && (b <= f && c <= f)) || (((a + d) <= f) && (b <= e && c <= e)) || (((b + c) <= f) && (a <= e && d <= e)) || (((a + c) <= f) && (b <= e && d <= e)) || (((b + d) <= e) && (a <= f && c <= f))) {
             g = true;
-        }else {
+        } else {
             g = false;
         }
 
-        if (g == true){
+        if (g == true) {
 
-           System.out.println("are placed");
-       }else {
-           System.out.println("aren't placed");
-       }
+            System.out.println("are placed");
+        } else {
+            System.out.println("aren't placed");
+        }
 
     }
 }
