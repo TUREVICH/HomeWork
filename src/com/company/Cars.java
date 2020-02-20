6c8abcd1e9cd2a99bdef547d770e076854d6ca94
TUREVICH <z.turevich@mail.ru>
2020-02-20 20:47:27 +0300

    change cars: add set & get

--- src/com/company/Cars.java
+++ src/com/company/Cars.java
@@ -2,14 +2,38 @@ package com.company;
 
 public class Cars {
 
-    String name;
-    int releaseDate;
+    private String name;
+    private int releaseDate;
     int tank;
     float consumption;
-    Float distance;
+    private Float distance;
+
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Float getDistance() {
+        return distance;
+    }
 
     public void ride(){
         distance = (tank / consumption) * 100;
     }
+
+
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,28 +15,29 @@ public class Main {
         Cars golf = new Cars();
         Cars transporter = new Cars();
 
-        golf.name = "Golf";
-        golf.releaseDate = 2002;
+
+        golf.setName("Golf");
+        golf.setReleaseDate(2002);
         golf.tank = 60;
         golf.consumption = 5.5f;
 
-        transporter.name = "Transporter";
-        transporter.releaseDate = 2013;
+        transporter.setName("Transporter");
+        transporter.setReleaseDate(2013);
         transporter.tank = 80;
         transporter.consumption = 8.6f;
 
         golf.ride();
-        System.out.println(golf.name + " will pass :" + golf.distance + " km.");
+        System.out.println(golf.getName() + " will pass :" + golf.getDistance() + " km.");
 
         transporter.ride();
-        System.out.println(transporter.name + " will pass :" + transporter.distance + " km.");
+        System.out.println(transporter.getName() + " will pass :" + transporter.getDistance() + " km.");
 
 
-        System.out.println( golf.name + " release date : " + golf.releaseDate + "\n" + transporter.name + " release date : " + transporter.releaseDate);
-        if (golf.releaseDate < transporter.releaseDate){
-            System.out.println(golf.name + " older then " + transporter.name);
+        System.out.println( golf.getName() + " release date : " + golf.getReleaseDate() + "\n" + transporter.getName() + " release date : " + transporter.getReleaseDate());
+        if (golf.getReleaseDate() < transporter.getReleaseDate()){
+            System.out.println(golf.getName() + " older then " + transporter.getName());
         }else {
-            System.out.println(transporter.name + " older then " + golf.name);
+            System.out.println(transporter.getName() + " older then " + golf.getName());
         }
     }
 }
