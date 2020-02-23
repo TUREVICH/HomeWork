491477dc44c2139b9f8e4d23ecb007405b679998
TUREVICH <z.turevich@mail.ru>
2020-02-23 17:53:28 +0300

    Task17 change

--- src/com/company/Task17.java
+++ src/com/company/Task17.java
@@ -4,13 +4,13 @@ public class Task17 {
 
     public static void main(String[] args) {
 
-        int vertical = 7;
-        int horizont = 7;
+        int vertical = 5;
+        int horizont = 5;
 
         int[][] array = new int[vertical][horizont];
 
 
-        for (int i = 0; i < (array.length / 2) + 1; i++){
+        for (int i = 0; i < array.length / 2 ; i++){
             for (int j = 0; j < array[i].length; j++){
                 if ((j < i) || (j >= (array[i].length - i)))
                     array[i][j]=0;
@@ -20,7 +20,7 @@ public class Task17 {
             }
 
         }
-        for (int i = array.length - 1; i >= array.length / 2 + 1; i--){
+        for (int i = array.length - 1; i >= array.length / 2  ; i--){
             for ( int j = 0; j < array[i].length; j++){
                 if ((j < (array[i].length - 1 - i)) || ( j > i))
                     array[i][j]=0;
