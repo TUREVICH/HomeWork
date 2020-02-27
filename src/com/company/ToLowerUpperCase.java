fbbd800a5a03cc9c29f93672c5855e25a8c3f718
TUREVICH <z.turevich@mail.ru>
2020-02-27 14:46:42 +0300

    change task ToLowerUpperCase

--- src/com/company/ToLowerUpperCase.java
+++ src/com/company/ToLowerUpperCase.java
@@ -1,59 +1,34 @@
 package com.company;
 
 public class ToLowerUpperCase {
-    static String string = "fsadSDg";
-    static char[][] alphabet = {{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'},
-                                {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}};
-    static String stringUpp = "";
-    static String stringLow = "";
+
+    static String string = "LoL, Кек";
+    static char[] chars = string.toCharArray();
 
     public static void main(String[] args) {
-
         toUpper();
         toLower();
-
     }
 
     public static void toUpper() {
-        for (int k = 0; k < string.length(); k++) {
-            for (int i = 0; i < alphabet.length; i++) {
-                for (int j = 0; j < alphabet[i].length; j++) {
-
-                    if (string.charAt(k) == alphabet[0][j]) {
-                        stringUpp = stringUpp + alphabet[1][j];
-                    } else if (string.charAt(k) == alphabet[1][j]) {
-                        stringUpp = stringUpp + string.charAt(k);
-                    }
-
-
-                }
-                break;
+        for (int i = 0; i < string.length(); i++) {
+            if ((chars[i] >= 'а') && (chars[i] <= 'я') || (chars[i] >= 'a') && (chars[i] <= 'z')) {
+                chars[i] -= 32;
             }
         }
-        System.out.println(stringUpp);
-
+        string = String.valueOf(chars);
+        System.out.println(string);
     }
 
     public static void toLower() {
-        for (int k = 0; k < string.length(); k++) {
-            for (int i = 0; i < alphabet.length; i++) {
-                for (int j = 0; j < alphabet[i].length; j++) {
-
-                    if (string.charAt(k) == alphabet[1][j]) {
-                        stringLow = stringLow + alphabet[0][j];
-                    } else if (string.charAt(k) == alphabet[0][j]) {
-                        stringLow = stringLow + string.charAt(k);
-                    }
-
-
-                }
-                break;
+        for (int i = 0; i < string.length(); i++) {
+            if ((chars[i] >= 'А') && (chars[i] <= 'Я') || (chars[i] >= 'A') && (chars[i] <= 'Z')) {
+                chars[i] += 32;
             }
         }
-        System.out.println(stringLow);
-
+        string = String.valueOf(chars);
+        System.out.println(string);
     }
-
 
 }
 
