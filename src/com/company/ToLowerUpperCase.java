bae6dc9ea820c9d2c11ce11b933a8927500a0a5a
TUREVICH <z.turevich@mail.ru>
2020-02-27 18:58:40 +0300

    change task ToLowerUpperCase

--- src/com/company/ToLowerUpperCase.java
+++ src/com/company/ToLowerUpperCase.java
@@ -2,15 +2,15 @@ package com.company;
 
 public class ToLowerUpperCase {
 
-    static String string = "LoL, Кек";
-    static char[] chars = string.toCharArray();
+    private static String string = "LoL, Кек";
+    private static char[] chars = string.toCharArray();
 
     public static void main(String[] args) {
         toUpper();
         toLower();
     }
 
-    public static void toUpper() {
+    private static void toUpper() {
         for (int i = 0; i < string.length(); i++) {
             if ((chars[i] >= 'а') && (chars[i] <= 'я') || (chars[i] >= 'a') && (chars[i] <= 'z')) {
                 chars[i] -= 32;
@@ -20,7 +20,7 @@ public class ToLowerUpperCase {
         System.out.println(string);
     }
 
-    public static void toLower() {
+    private static void toLower() {
         for (int i = 0; i < string.length(); i++) {
             if ((chars[i] >= 'А') && (chars[i] <= 'Я') || (chars[i] >= 'A') && (chars[i] <= 'Z')) {
                 chars[i] += 32;
